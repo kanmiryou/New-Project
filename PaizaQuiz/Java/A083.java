7ac65b35f382dcb713463a21211f22099b9d131c
YOSHIYA <46104562+kanmiryou@users.noreply.github.com>
2024-08-27 17:51:38 +0900

    Update A083.java

--- PaizaQuiz/Java/A083.java
+++ PaizaQuiz/Java/A083.java
@@ -9,25 +9,34 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        
+
+        // 標準入力から整数Nを取得
         int N = sc.nextInt();
 
         // 商品をすべてリストに追加する
         List<Integer> items = new ArrayList<Integer>();
         for (int i = 0; i < N; i++) {
             int price = sc.nextInt();
+
+            // 10000円の商品があった場合、最大金額を表示し処理を終了する
             if (price == 10000) {
                 System.out.println(Integer.toString(maxPrice));
                 return ; 
             }
             items.add(price);
         }
+
+        // 最大金額計算処理を実行する
         getMaxPrice(items, N);
     }
 
-　　// 最大金額を求める処理
-　　// 商品リストを全bit探索することによりすべての組み合わせを求める
-    // 最大金額10000円の組み合わせがあった場合、処理を終了する
+    
+　　/** 最大金額計算処理
+    * 商品リストを全bit探索することによりすべての組み合わせを求める
+    * 最大金額10000円の組み合わせがあった場合、処理を終了する
+    * @param List<Integer> items 商品リスト
+    * @param int N 商品数
+    */
     static void getMaxPrice(List<Integer> items, int N) {
 
         // 商品数の2乗分、ループを回す
@@ -37,20 +46,27 @@ public class Main {
             // 全bit探索の処理
             for (int j = 0; j < N; j++) {
                 if ((1&i>>j) == 1) {
+                    // 商品を購入する場合、金額を足す
                     tmp += items.get(j);
                 }
             }
 
-            // 10000円が出た時点で処理を終了する
+            // 10000円が出た場合
             if (tmp == 10000) {
+
+                // 処理を終了する
                 System.out.println(Integer.toString(tmp));
                 return;
 
-            // それ以外の場合、最大金額を更新していく
+            // 金額が1000円より低い、かつ最大金額より高い場合
             } else if( tmp < 10000 && tmp > maxPrice) {
+
+                // 最大金額を更新する
                 maxPrice = tmp;
             }
         }
+
+        // 最大金額を表示する
         System.out.println(Integer.toString(maxPrice));
     }
     
